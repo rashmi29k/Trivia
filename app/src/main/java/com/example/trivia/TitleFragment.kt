3ac05bd8f5263b8a80d51756a8a5c8b5770bff38
Rashmi <rashmi29491@gmail.com>
2022-01-26 15:31:29 +0530

    adding

--- app/src/main/java/com/example/trivia/TitleFragment.kt
+++ app/src/main/java/com/example/trivia/TitleFragment.kt
@@ -12,10 +12,6 @@ class TitleFragment : Fragment() {
     private var _binding: FragmentTitleBinding? = null
     private val binding get() = _binding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?

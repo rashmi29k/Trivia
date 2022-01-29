565813831aaf4554f566c9a44fc54f1cbea83c62
Rashmi <rashmi29491@gmail.com>
2022-01-29 19:36:49 +0530

    add

--- app/src/main/java/com/example/trivia/GameWonFragment.kt
+++ app/src/main/java/com/example/trivia/GameWonFragment.kt
@@ -1,10 +1,8 @@
 package com.example.trivia
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.trivia.databinding.FragmentGameWonBinding
@@ -14,17 +12,43 @@ class GameWonFragment : Fragment() {
 
     private var _binding: FragmentGameWonBinding? = null
     private val binding get() = _binding
-
+    val args = GameWonFragmentArgs.fromBundle(requireArguments())
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         _binding= FragmentGameWonBinding.inflate(inflater,container, false)
-        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+
         binding?.nextMatchButton?.setOnClickListener {
             it.findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment(args.numQuestions, args.numCorrect))
         }
 
-
-        Toast.makeText(context, "number: ${args.numCorrect}", Toast.LENGTH_LONG).show()
+        setHasOptionsMenu(true)
         return binding?.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getShareIntent(): Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text,  args.numQuestions, args.numCorrect))
+        return shareIntent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
     }
 }
--- app/src/main/java/com/example/trivia/MainActivity.kt
+++ app/src/main/java/com/example/trivia/MainActivity.kt
@@ -2,27 +2,33 @@ package com.example.trivia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.trivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        drawerLayout = binding.drawerLayout
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
